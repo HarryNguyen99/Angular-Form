b5a8ffb1e7cc51867e8978ccb81b249abcfefd12
Harry Nguyen <50328363+HarryNguyen99@users.noreply.github.com>
2020-07-07 10:48:34 +0700

    signin, signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
 import {ReactiveFormsModule} from '@angular/forms';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
+import { SignupFinalComponent } from './signup-final/signup-final.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent
+    TodoComponent,
+    SigninComponent,
+    SignupComponent,
+    TemplateDrivenFormComponent,
+    SignupFinalComponent
   ],
     imports: [
         BrowserModule,
---
+++ src/app/signin/signin.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.css']
+})
+export class SigninComponent implements OnInit {
+
+  loginForm: FormGroup;
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.loginForm);
+  }
+
+
+}
